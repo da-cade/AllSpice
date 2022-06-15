12529faf6abafb2812af73e7dd3745edf722f06e
Dylan Cade <dacade.code@gmail.com>
2022-06-15 10:09:11 -0600

    deploooooooooooooooooy

---
+++ allSpice/wwwroot/assets/AboutPage.083153db.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.8b11d808.js";import{o,b as t,e as s}from"./vendor.79e61492.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,u,f,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
